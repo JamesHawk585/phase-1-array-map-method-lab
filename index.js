e7a0949ee5d9c4d66894f7681d29ac767507ade5
James Hawk <jph94880@gmail.com>
2023-04-16 10:55:54 -0500

    Adds my file.md

--- index.js
+++ index.js
@@ -11,6 +11,18 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
-}
+  const titleCased = () => {
+    return tutorials.map(function(title) {
+      return title.split(' ').map(function(word) {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      }).join(' ');
+    });
+  }
+  
+
+
+const titleCasedTutorials = titleCased();
+
+console.log(titleCasedTutorials);
+
+
